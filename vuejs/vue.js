0b9c31cabdfb52318de1f5fca5a56a7eb3c34166
Bhupinder1989 <bhupinderdesigner1989@gmail.com>
2019-01-06 13:51:25 +0530

    first commit

---
+++ vuejs/vue.js
@@ -0,0 +1,196 @@
+// var app = new Vue ({
+//   el: '#app', 
+// data: {
+//   message:'hello World'
+// }
+// })
+
+
+// var app2 = new Vue ({
+//   el: '#app2',
+//   data: {
+//     message:'You loaded this page'
+//   }
+// })
+
+
+// var app3= new Vue({
+//   el:'#app3',
+//   data: {
+//     seen:true
+//   }
+// })
+
+
+// var app4 = new Vue({
+//   el:'#app4',
+//   data:{
+//     todos:[
+//       {text:'1'},
+//       {text:'2'},
+//       {text:'3'}
+//     ]
+//   }
+// })
+
+
+
+// var app5 = new Vue({
+//   el:'#app5',
+//   data: {
+//     message:'Hello Bhupinder'
+//   },
+//   methods: {
+//     reverseMessage: function() {
+//       this.message=this.message.split('').reverse().join('')
+//     }
+//   }
+// })
+
+
+
+// var app6 = new Vue({
+//   el:'#app6',
+//   data:{
+//     message:'Hello Bhupinder'
+//   }
+// })
+
+
+// // Vue Component 
+
+
+
+
+// Vue.component('todo-item',{
+//   props:['todo'],
+// template:'<li>{{todo.text}}</li>'
+// })
+
+
+
+// var app7 = new Vue({
+//   el:'#app7',
+//   data:{
+//     vegList:[
+//       {id:0, text:'Potato'},
+//       {id:1, text:'Onion'}
+//     ]
+//   }
+// })
+
+
+
+
+
+// var app8 = new Vue({
+//   el:'#app8',
+//   data:{
+//     htmlContent:'<p>Hey Bhupinder Singh</p>',
+//     name:'Guru',
+//     className:'imgClass',
+//     txtName:'Buupinder',
+//     txtEmail:'bhupin@gmail.com'
+//   }
+// })
+
+
+
+// var app9 = new Vue({
+//   el:'#app9',
+//   data:{
+//     counter: 0
+//   },
+// methods: {
+//   increment: function(inc) {
+//     this.counter=this.counter + inc;
+//     },
+//     decrement: function(desc) {
+//       this.counter=this.counter - desc;
+//       }
+// }
+// })
+
+
+
+
+// var app10 = new Vue({
+//   el:'#app10',
+//   data: {
+//     x:0,
+//     y:0
+//   },
+//   methods: {
+//     mouseOver: function(event) {
+
+// this.x = event.offsetX;
+// this.y = event.offsetY;
+//     },
+//     mouseoverfunction: function() {
+// console.log('Hey this is mouse over event');
+//     },
+//     mouseoutfunction: function() {
+// console.log('Events is fire on Mouse out Btn')
+//     }
+//   }
+// })
+
+
+
+// var app11 = new Vue({
+//   el:'#app11',
+//   data: {
+
+//   },
+//   methods: {
+//     keypressfunction: function(event) {
+//       console.log(event.key);
+//     }
+//   }
+// })
+
+
+
+// var app12 = new Vue({
+//   el:'#app12',
+//   data: {
+// counter: 0
+//   },
+//   methods: {
+//     incrementfunction: function() {
+//       this.counter++;
+//     },
+//     preventFunction: function() {
+//       console.log('prevet the anchor link');
+//     },
+//     stopformsubmit: function() {
+// console.log('form is submitted');
+//     }
+//   }
+// })
+// var app13 = new Vue({
+//   el:'#app13',
+//   data: {
+
+//   },
+//   methods: {
+//     keypressfunction() {
+//       console.log('Key is pressed')
+//     }
+//   }
+// })
+
+
+
+var app14 = new Vue({
+  el:'#app14',
+  data: {
+    character:"",
+    name:"bhupiner",
+    email:"bhupindersinghmca2@gmail.com"
+  },
+  methods: {
+
+  }
+})
+
